ca08dd35841be7cd46a8042c78e3f67bfb960b55
kantarutti <marco.cantarutti@gmail.com>
2026-07-07 15:28:27 +0200

    Feat: screens.dart - Aggiunte schermate di autenticazione

---
+++ lib/screens/screens.dart
@@ -0,0 +1,8 @@
+// Esporta tutte le schermate
+export 'home/home_screen.dart';
+export 'eventi/eventi_screen.dart';
+export 'gruppi/gruppi_screen.dart';
+export 'spese/spese_screen.dart';
+export 'partecipanti/partecipanti_screen.dart';
+export 'auth/login_screen.dart';
+export 'auth/register_screen.dart';
